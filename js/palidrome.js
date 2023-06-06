dd30ff28e25c46aa1088dbbb8a93a9fbc207a4ad
Andrea Creazzi <andreacreazzi@libero.it>
2023-06-06 15:08:54 +0200

    add html-js (polindrome complete

--- js/palidrome.js
+++ js/palidrome.js
@@ -1,2 +1,27 @@
 // controllo js
-console.log(`JS OK`)
+console.log(`JS OK`);
+
+// funzione parola polindroma
+function wordPalindrome(word) {
+
+    // lungezza della parola
+    const len = word.length;
+
+    // ciclo per la parola
+    for (let i = 0; i < len / 2; i++) {
+
+        // controllo della lettera inziale e finale
+        if (word[i] !== word[len - 1 - i]) {
+            return 'non è un nome polindromo';
+        }
+    }
+    return 'è un nome polindromo';
+}
+
+// risposta dell'utente
+const word = prompt('Enter a string: ');
+
+// richiamo della funzione
+const result = wordPalindrome(word);
+
+console.log(result);
